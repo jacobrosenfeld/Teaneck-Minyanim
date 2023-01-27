d1bebd0fa13d0d4ec9c5783d15752c85cba5014d
Jacob Rosenfeld <me@jacobrosenfeld.me>
2023-01-26 20:39:01 -0500

    beanstock

--- src/main/java/com/tbdev/teaneckminyanim/TeaneckMinyanimApplication.java
+++ src/main/java/com/tbdev/teaneckminyanim/TeaneckMinyanimApplication.java
@@ -1,17 +1,27 @@
 package com.tbdev.teaneckminyanim;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.jdbc.core.JdbcTemplate;
 
+import javax.sql.DataSource;
+import java.sql.Time;
 import java.util.TimeZone;
 
 @SpringBootApplication
-@EnableJpaRepositories
-public class TeaneckMinyanimApplication {
+public class TeaneckMinyanimApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TeaneckMinyanimApplication.class, args);
         TimeZone.setDefault(TimeZone.getTimeZone("America/New_York"));
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TeaneckMinyanimApplication.class);
     }
 }
