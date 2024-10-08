e47c8c335460b899307b4ad6928bedff224333f5
Jacob Rosenfeld <me@jacobrosenfeld.me>
2024-10-08 01:42:03 -0400

    typo in mv

--- src/main/java/com/tbdev/teaneckminyanim/front/controllers/ZmanimController.java
+++ src/main/java/com/tbdev/teaneckminyanim/front/controllers/ZmanimController.java
@@ -445,7 +445,7 @@ public class ZmanimController {
         mv.getModel().put("shacharisMinyanim", shacharisMinyanim);
         mv.getModel().put("minchaMinyanim", minchaMinyanim);
         mv.getModel().put("maarivMinyanim", maarivMinyanim);
-        m.getModel().put("selichosMinyanim", selichosMinyanim);
+        mv.getModel().put("selichosMinyanim", selichosMinyanim);
 
         return mv;
     }
