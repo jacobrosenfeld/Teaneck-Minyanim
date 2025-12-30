a8802d234d8056e453446f3719a77dcba140f5de
Jacob Rosenfeld <me@jacobrosenfeld.me>
2025-12-30 11:17:26 -0500

    Update page titles to include dynamic site name for consistency - 1.3.5

--- src/main/java/com/tbdev/teaneckminyanim/controllers/AdminController.java
+++ src/main/java/com/tbdev/teaneckminyanim/controllers/AdminController.java
@@ -1205,7 +1205,6 @@ public class AdminController {
         Map<String, String> locationNames = new HashMap<>();
         for (Minyan minyan : minyanim) {
             Location location = this.locationDAO.findById(minyan.getLocationId());
-//            TODO: DONT JUST SHOW EMPTY STRING
             String locationDisplayName = location == null ? "" : location.getName();
             locationNames.put(minyan.getId(), locationDisplayName);
         }
