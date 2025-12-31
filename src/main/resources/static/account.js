6347db5bd4e3fb8a2c87a077232995cb5e5904e0
Jacob Rosenfeld <me@jacobrosenfeld.me>
2025-12-31 14:37:54 -0500

    Refactor modal trigger functions in account.js for consistency and clarity

--- src/main/resources/static/account.js
+++ src/main/resources/static/account.js
@@ -4,18 +4,23 @@ function showChangePasswordModal() {
 }
 
 $(function () {
+    // Initialize tooltips
     $('[data-toggle="tooltip"]').tooltip();
 
-    $('#open-disable-account-modal').on('click', function () {
+    // Explicitly bind modal triggers to ensure they work after jQuery/Bootstrap load
+    $('#open-disable-account-modal').on('click', function (e) {
+        e.preventDefault();
         $('#disable-account-modal').modal('show');
     });
 
-    $('#open-enable-account-modal').on('click', function () {
+    $('#open-enable-account-modal').on('click', function (e) {
+        e.preventDefault();
         $('#enable-account-modal').modal('show');
     });
 
-    $('#open-delete-account-modal').on('click', function () {
+    $('#open-delete-account-modal').on('click', function (e) {
         if (!$(this).hasClass('disabled')) {
+            e.preventDefault();
             $('#delete-account-modal').modal('show');
         }
     });
